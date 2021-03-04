dd4046caeeed9cd89311385bbce0cd21ac70af8a
Joseph Eoff <joseph.eoff@molear.de>
2021-03-04 21:28:57 +0100

    Moved secret key out of settings.py

--- mud_pi/mud_pi/settings.py
+++ mud_pi/mud_pi/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@b29fedoiewo^dj#j*tu*ist89h#8jln6ccsiewh+ch)mx(*_%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -121,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from .local_settings import *
--- mud_pi/zones/models.py
+++ mud_pi/zones/models.py
@@ -4,3 +4,4 @@ class Zones(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField()
     zonePolygon = models.PolygonField()
+    #Need to link items into here somehow
