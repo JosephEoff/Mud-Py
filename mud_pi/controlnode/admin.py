03fa3e821ce7e03640dab9d49d64b2779a40da5b
Joseph Eoff <joseph.eoff@molear.de>
2021-03-05 22:49:35 +0100

    Added controlnode data logging.

--- mud_pi/controlnode/admin.py
+++ mud_pi/controlnode/admin.py
@@ -1,5 +1,11 @@
 from django.contrib.gis import admin
 from .models import ControlNode
+from .models import ControlNodeUnit
+from .models import ControlNodeType
+from .models import ControlNodeData
 from leaflet.admin import LeafletGeoAdmin
 
 admin.site.register(ControlNode, LeafletGeoAdmin)
+admin.site.register(ControlNodeUnit)
+admin.site.register(ControlNodeType)
+admin.site.register(ControlNodeData)
--- mud_pi/controlnode/models.py
+++ mud_pi/controlnode/models.py
@@ -8,3 +8,26 @@ class ControlNode(models.Model):
     
     def __str__(self):
         return self.name
+        
+class ControlNodeUnit(models.Model):
+    name = models.CharField(max_length = 50)
+    abbreviatedName = models.CharField(max_length = 5)
+    
+    def __str__(self):
+        return self.name
+        
+class ControlNodeType(models.Model):
+    typeName = models.CharField(max_length = 50)
+    unit = models.ForeignKey(ControlNodeUnit,  on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.typeName
+    
+class ControlNodeData(models.Model):
+    sensor =  models.ForeignKey(ControlNode,  on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    datatype = models.ForeignKey(ControlNodeType,  on_delete=models.CASCADE)
+    value = models.FloatField()
+    
+    def __str__(self):
+        return "Timestamp: " +str(self.timestamp) + " Type:" + str(self.datatype.typeName) + " Value:" + str(self.value) +  str(self.datatype.unit.abbreviatedName)
